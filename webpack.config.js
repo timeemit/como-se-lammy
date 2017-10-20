6a15f148c5dab9bffe14cc7d6929f40520062144
TimeEmit <LiamNorris1231853211@gmail.com>
2017-10-19 21:18:28 -0700

    Webpack: Config

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "./docs/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              query: {
+                minimize: true
+              }
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./docs/fonts/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "./docs/images/"
+            }
+          },
+          "image-webpack-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("docs/bundle.css"),
+    new UglifyJSPlugin()
+  ]
+}
