e75a9bd017c5abd51566cc8891b41bb3a33607a8
TimeEmit <LiamNorris1231853211@gmail.com>
2017-10-21 12:09:15 -0700

    Webpack Config: .ico

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
         use: [
           {
             loader: "file-loader",
